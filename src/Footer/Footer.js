30c4ce7e1082a5793aa27dd79565a8207939a81a
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 19:20:47 +0200

    create Footer Component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+class Footer extends React.Component{
+
+    
+    
+    render(){
+        return (
+
+            <>
+            
+            <div class="footer">
+        <div class="footer-columns">
+            <div class="footer-column">
+                <h3>Column 1</h3>
+                <p>Content for Column 1.</p>
+            </div>
+            <div class="footer-column">
+                <h3>Column 2</h3>
+                <p>Content for Column 2.</p>
+            </div>
+            <div class="footer-column">
+                <h3>Column 3</h3>
+                <p>Content for Column 3.</p>
+            </div>
+        </div>
+    </div>
+
+            
+            
+            </>
+        )
+    }
+}
+
+
+
+
+export default Footer;
