140e2c45b9a2807d406074c56424670d21858782
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 21:26:54 +0200

    edit of footer components

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./style.css";
 import ButtonBlack from "../Buttons/ButtonBlack";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faMobilePhone } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faFacebookF, faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+
 
 class Footer extends React.Component{
 
@@ -15,21 +19,20 @@ class Footer extends React.Component{
         <div class="footer-columns">
             <div class="footer-column">
                 <h3>GET IN TOUCH</h3>
-                <p>Abdelrahmanshaaban998@gmail.com</p>
-                <p>(+20) 010 000 111 000</p>
+                <p><FontAwesomeIcon icon={faEnvelope} /> abdelrahmanshaaban998@gmail.com</p>
+                <p><FontAwesomeIcon icon={faMobilePhone} /> (+20) 010 000 111 000</p>
             </div> 
             <div class="footer-column">
             <ButtonBlack name="CONTACT ME" textcolor="white" backgroundcolor="Transparent" size="18px" padding="15px" cursor="pointer" border_radius="4px" border="2px solid white" />
-
+ 
             </div>
             <div class="footer-column">
-                <h3>Column 3</h3>
-                <p>Content for Column 3.</p>
+                <h3><FontAwesomeIcon icon={faFacebookF} /> <FontAwesomeIcon icon={faSquareXTwitter} />  </h3>
+                <p>copywrite @2024</p>
             </div>
         </div>
     </div>
 
-            
             
             </>
         )
