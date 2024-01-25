a715e3b39a660531892c556283fa5c21e1db55bd
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 19:17:21 +0200

    modify Portfolio components

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,10 +1,48 @@
 import React from "react";
+import "./style.css";
+import CartBlack from "../Carts/CartBlack";
 
 
 class Portfolio extends React.Component{
 
     
 
+   
+    render(){
+        return(
+            <>
+    
+            
+  <div class="portfolio-section">
+    <div class="portfolio-box">
+      <h2>Project 1</h2>
+        <hr></hr>
+    </div>
+
+    
+
+    <CartBlack color="grey" name="Hello world"/>
+
+    <div class="portfolio-box">
+      <h2>Project 3</h2>
+      <hr></hr>
+    </div>
+
+    <CartBlack color="grey" name="good noon"/>
+
+    <div class="portfolio-box">
+      <h2>Project 5</h2>
+      <hr></hr>
+    </div>
+
+    <CartBlack color="grey" name="good morning"/>
+
+  </div>
+
+
+            </>
+        )
+    }
     
 }
 
