f173f1409a3bc9c005234e5bd988f8d52f94781f
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 19:01:19 +0200

    added AboutMe component in the App.js view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HeroSection from './Header/HeroSection';
+import AboutMe from './About/AboutMe';
 
 function App() {
   return (
     <div className="App">
         <HeroSection />
+        <AboutMe />
     </div>
   );
 }
