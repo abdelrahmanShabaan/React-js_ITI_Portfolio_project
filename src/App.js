01d750687a5103504dbe48cf3dc7b727a9177ebd
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 19:12:08 +0200

    add component Skills to App Component and show in webpage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HeroSection from './Header/HeroSection';
 import AboutMe from './About/AboutMe';
+import Skills from './Skills/Skills';
 
 function App() {
   return (
     <div className="App">
         <HeroSection />
         <AboutMe />
+        <Skills />
     </div>
   );
 }
