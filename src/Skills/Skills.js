e52bed8278c695a9e41f57e14d2c27906cbcf66d
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 19:10:31 +0200

    modify for Component of Skills

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -3,9 +3,46 @@ import "./style.css";
 
 
 class Skills extends React.Component{
-   
 
+    
+    render(){
+        return(
+            <>
 
+<section class="skill-me-section">
+        <div class="left-columns">
+            <h1>Focuss on</h1>
+            <p>Software engineering</p>
+            <p>Software engineering</p>
+            <p>Software engineering</p>
+            <p>Software engineering</p>
+        </div>
+        <div class="right-columns">            
+                <p>HTML</p>
+                <div class="container">
+                <div class="skills html">90%</div>
+                </div>
+
+                <p>CSS</p>
+                <div class="container">
+                <div class="skills css">80%</div>
+                </div>
+
+                <p>JavaScript</p>
+                <div class="container">
+                <div class="skills js">65%</div>
+                </div>
+
+                <p>PHP</p>
+                <div class="container">
+                <div class="skills php">60%</div>
+                </div>
+        </div>
+        </section>
+
+            </>
+        )
+    }
     
 }
 
