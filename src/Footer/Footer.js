c1a193dc1bf7a98c35c9c238e2337e1ff893ccc8
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 20:48:06 +0200

    start edit with fotter

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-
+import ButtonBlack from "../Buttons/ButtonBlack";
 
 class Footer extends React.Component{
 
@@ -14,12 +14,13 @@ class Footer extends React.Component{
             <div class="footer">
         <div class="footer-columns">
             <div class="footer-column">
-                <h3>Column 1</h3>
-                <p>Content for Column 1.</p>
-            </div>
+                <h3>GET IN TOUCH</h3>
+                <p>Abdelrahmanshaaban998@gmail.com</p>
+                <p>(+20) 010 000 111 000</p>
+            </div> 
             <div class="footer-column">
-                <h3>Column 2</h3>
-                <p>Content for Column 2.</p>
+            <ButtonBlack name="CONTACT ME" textcolor="white" backgroundcolor="Transparent" size="18px" padding="15px" cursor="pointer" border_radius="4px" border="2px solid white" />
+
             </div>
             <div class="footer-column">
                 <h3>Column 3</h3>
